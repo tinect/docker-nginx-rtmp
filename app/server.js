7f4e1a22b61292dc1a0916031f592fe0064b7d60
tinect <s.koenig@tinect.de>
2021-01-03 18:59:45 +0100

    ...

--- app/server.js
+++ app/server.js
@@ -5,37 +5,18 @@ fastify.register(require('fastify-formbody'));
 let processes = [];
 
 fastify.post('/start', async (request, reply) => {
+    let parts = request.body.name.split('__');
+    let url = parts[0] + '://' + parts[1] + '/live/' + parts[2] + '/' + parts[3] + '/video.m3u8';
 
-    let url = request.body.name;
-
-    const searchParams = new URLSearchParams()
-
-    if (request.body['token']) {
-        searchParams.append('token', request.body['token'])
-    }
-
-    if (request.body['sd']) {
-        searchParams.append('sd', request.body['sd'])
-    }
-
-    if (request.body['rebase']) {
-        searchParams.append('rebase', request.body['rebase'])
-    }
-
-    if (searchParams.toString() !== '') {
-        url += '?' + searchParams.toString();
-    }
-
-    processes[request.body.name] = spawn('/usr/local/bin/ffmpeg', ['-i', url, '-map', '0', '-sn', '-c:v', 'copy', '-f', 'flv', 'rtmp://localhost:1935/live/' + url]);
-
-    /*processes[request.body.name].stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
+    processes[request.body.name] = spawn('/usr/local/bin/ffmpeg', ['-i', url, '-map', '0', '-sn', '-c:v', 'copy', '-f', 'flv', 'rtmp://localhost:1935/live/' + request.body.name]);
 
     processes[request.body.name].stderr.on('data', (data) => {
         console.error(`stderr: ${data}`);
     });
 
+    /*processes[request.body.name].stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
     processes[request.body.name].on('close', (code) => {
         console.log(`child process exited with code ${code}`);
     });*/
